cc23dc31846ddbc96821b7abac3cc1bafce9740c
Zach rose <zachrose@ymail.com>
2021-07-10 12:56:22 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-// Assignment code here
+var generateBtnEl = document.querySelector("#generate")
 
+const randomChar = {
+    lower: randomLower,
+    upper: randomUpper,
+    number: randomNum,
+    symbol: randomSym
+}
+generateBtnEl.addEventListener("click", () => {
+    prompt("Please choose a number between 1 and 128."),
+    confirm("Please choose 'OK' to select lowercase and 'Cancel' to un-select lowercase"),
+    confirm("Please choose 'OK' to select uppercase and 'Cancel' to un-select uppercase"),
+    confirm("Please choose 'OK' to select numbers and 'Cancel' to un-select numbers"),
+    confirm("Please choose 'OK' to select symbols and 'Cancel' to un-select symbols")
+});
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function randomLower() {
+    const lower = 'abcdefghijklmnopqrstuvwxyz'
+    return lower[Math.floor(Math.random() * lower.length)]
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function randomUpper() {
+    const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return upper[Math.floor(Math.random()* upper.length)]
+}
+
+function randomNum() {
+    const number = '123456789'
+    return number[Math.floor(Math.random() * number.length)]
+}
+
+function randomSym() {
+    const symbol = '!@#$%^&*'
+    return symbol[Math.floor(Math.random() * symbol.length)]
+}
+console.log(randomSym())
